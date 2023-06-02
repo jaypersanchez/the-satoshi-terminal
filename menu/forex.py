efc34c04772d4465558a84f01b7e1f311c5767d8
Jayper <jaypersanchez@gmail.com>
2023-06-02 12:07:29 +0800

    commiting changes from the end of day work

--- menu/forex.py
+++ menu/forex.py
@@ -6,7 +6,7 @@ import sys
 import pandas as pd
 from openbb_terminal.sdk import openbb
 # Import necessary libraries
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QLabel, QTableWidget, QTableWidgetItem
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QSize
 
@@ -14,28 +14,45 @@ class Forex(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-        #self.stockEvents()
         
     def initUI(self):
-        print('You clicked the Forex button')
-        # Create a new window
-        #stockWindow = QWidget()
-
         # Set size and position of the window
         self.resize(500, 500)
         self.move(800, 200)
         self.setWindowTitle('Forex Commodities')
         self.show()
+        
+        # add the button
+        self.search = QPushButton("Forex Pair Quote", self)
+        self.search.setToolTip('Forex Pair Quote')
+        self.search.clicked.connect(self.forexQuote)
+        # set the layout
+        self.hbox = QHBoxLayout()
+        self.hbox.addStretch(1)
+        self.hbox.addWidget(self.search)
+        self.hbox.addStretch(1)
+        self.setLayout(self.hbox)
             
         
-    def stockSearch(self):
-        columns = ['name', 'country', 'sector', 'industry_group', 'industry', 'exchange']
-        #stocks_df = pd.DataFrame(openbb.stocks.search(country="United States", exchange_country="Germany")) #this will open OpenBB dataframe
-        #stocks_df = openbb.stocks.search(country="United States", exchange_country="Germany")
-        stocks_df = openbb.stocks.search(country="United States", exchange_country="Canada")
-        stocks_dict = stocks_df.to_dict()
-        stocks_dict['title'] = "Satoshi Terminal"
-        new_df = pd.DataFrame(stocks_dict)
-        print(new_df[columns])
+    def forexQuote(self):
+        forex_df = pd.DataFrame(openbb.forex.quote("EURUSD"))
+        forex_df.head()
+        print(forex_df)
+        self.show()
+        
+        self.label = QLabel("Forex Pair Quote", self)
+        self.table = QTableWidget(self)
+        self.table.setColumnCount(4)
+        self.table.setRowCount(len(forex_df))
+        for i in range(len(forex_df)):
+            for j in range(4):
+                if j < forex_df.shape[1]:
+                     self.table.setItem(i, j, QTableWidgetItem(str(
+                    forex_df.iloc[i][j])))
+
+        self.setLayout(self.hbox)
+        self.layout().addWidget(self.label)
+        self.layout().addWidget(self.table)
+        self.show()
 
         
